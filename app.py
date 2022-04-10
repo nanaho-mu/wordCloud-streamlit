4164c6de771eb0878f8878761afb017569b36619
nanaho-mu <nanaho.mu0823@gmail.com>
2022-04-10 13:19:07 +0900

    create_task関数の作成

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 from PIL import Image
 from wordcloud import WordCloud
+
+def create_task(count=3):
+    texts=[]
+    for i in range(count):
+        text=[]
+        text.append(st.sidebar.text_input("task"+str(i+1), "task"+str(i+1)))
+        slider=st.sidebar.slider("task"+str(i+1)+"の脳内比率", 0, 10, 5)
+        text=text*slider
+        text=" ".join(text)
+        texts.append(text)
+    return texts
