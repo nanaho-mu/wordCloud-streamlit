7a8377984edf28e5e55ee2afcf87701e4e66da0f
nanaho-mu <nanaho.mu0823@gmail.com>
2022-04-10 13:22:27 +0900

    add_task関数の作成

--- app.py
+++ app.py
@@ -12,3 +12,14 @@ def create_task(count=3):
         text=" ".join(text)
         texts.append(text)
     return texts
+
+def add_task(count):
+    add_texts=[]
+    for i in range(3, count+3):
+        text=[]
+        text.append(st.sidebar.text_input("task"+str(i+1), "task"+str(i+1)))
+        slider=st.sidebar.slider("task"+str(i+1)+"の脳内比率", 0, 10, 5)
+        text=text*slider
+        text=" ".join(text)
+        add_texts.append(text)
+    return add_texts
